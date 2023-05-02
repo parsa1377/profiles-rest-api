c01f7e72824d4f8930da723d312ebf86666de6c0
Parsa <parsaostadnazari@gmail.com>
2023-05-02 04:59:14 +0430

    Enable django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+# ....................... #
+
 class UserProfileManager(BaseUserManager):
     """Helps Django work with our custom user model."""
 
@@ -31,28 +33,34 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system"""
+    """
+    Represents a "user profile" inside out system. Stores all user account
+    related data, such as 'email address' and 'name'.
+    """
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserProfileManager()        
-    """We will create this class later"""
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """Django uses this when it needs to get the user's full name."""
+
         return self.name
 
     def get_short_name(self):
-        """Retrieve short name of user"""
+        """Django uses this when it needs to get the users abbreviated name."""
+
         return self.name
 
     def __str__(self):
-        """Return string representation of our user"""
+        """Django uses this when it needs to convert the object to text."""
+
         return self.email
